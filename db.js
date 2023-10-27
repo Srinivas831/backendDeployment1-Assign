4ae04f0f89804fb8bf97496e49e3d5a0e6f92460
Srinivas831 <hsrinivas802@gmail.com>
2023-10-27 20:13:14 +0530

    deployment of backend

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require("mongoose");
 require("dotenv").config();
-const connection=mongoose.connect(process.env.mongoLink);
+const connection=mongoose.connect("mongodb+srv://srinivasan:srinivashari@cluster0.gmtgxdz.mongodb.net/notesBackend?retryWrites=true&w=majority");
 
 module.exports=connection;
